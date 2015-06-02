90d562bcfa34075e8453de4f5cde6622db1ff57e
Ryan Carver <ryan@ryancarver.com>
2015-06-02 10:20:22 -0700

    more better maybe

--- repack.go
+++ repack.go
@@ -21,13 +21,13 @@ func newCounter(name string) *counter {
 }
 
 // Inc adds one to the "in" count.
-func (c *counter) Inc() {
-	c.In++
+func (c *counter) Inc(a int) {
+	c.In += a
 }
 
 // Dec adds one to the "out" count.
-func (c *counter) Dec() {
-	c.Out++
+func (c *counter) Dec(a int) {
+	c.Out += a
 }
 
 // Missing is the difference between in and out.
@@ -60,11 +60,11 @@ func (w *warehouse) Unpack(in <-chan *truck) {
 			pallets: make([]pallet, 0, len(t.pallets)),
 		}
 		w.trucks <- *emptyTruck
-		w.truckCounter.Inc()
+		w.truckCounter.Inc(1)
 		for _, p := range t.pallets {
-			w.palletCounter.Inc()
+			w.palletCounter.Inc(1)
 			for _, b := range p.boxes {
-				w.boxCounter.Inc()
+				w.boxCounter.Inc(1)
 				w.boxes <- b
 			}
 		}
@@ -73,13 +73,11 @@ func (w *warehouse) Unpack(in <-chan *truck) {
 
 // PackTruck re-packs a truck as efficiently as possible.
 func (w *warehouse) PackTruck(t *truck) {
-	w.truckCounter.Dec()
+	w.truckCounter.Dec(1)
 	for len(t.pallets) < cap(t.pallets) {
-		p := &pallet{
-			boxes: make([]box, 0, 16),
-		}
-		packPallet(p, w)
-		w.palletCounter.Dec()
+		p := packOnePallet(w.boxes)
+		w.palletCounter.Dec(1)
+		w.boxCounter.Dec(len(p.boxes))
 		t.pallets = append(t.pallets, *p)
 	}
 }
@@ -88,24 +86,50 @@ func (w *warehouse) PackTruck(t *truck) {
 // efficiency.
 func (w *warehouse) PackRemainingBoxes(t *truck) {
 	close(w.boxes)
-	w.truckCounter.Dec()
-	for b := range w.boxes {
-		p := &pallet{
-			boxes: []box{b},
-		}
-		w.boxCounter.Dec()
-		w.palletCounter.Dec()
+	w.truckCounter.Dec(1)
+	pallets := packAllBoxes(w.boxes)
+	for _, p := range pallets {
+		w.palletCounter.Dec(1)
+		w.boxCounter.Dec(len(p.boxes))
 		t.pallets = append(t.pallets, *p)
 	}
 }
 
-func chooseBox(boxes []*box, usedBoxes map[uint32]bool) *box {
-	for _, b := range boxes {
-		if !usedBoxes[b.id] {
-			return b
-		}
+func packOnePallet(boxes chan box) *pallet {
+	var boxCap int
+	if maxBoxes > len(boxes) {
+		boxCap = len(boxes)
+	} else {
+		boxCap = maxBoxes
 	}
-	return nil
+	packer := &palletPacker{
+		boxes:     make([]*box, 0, boxCap),
+		usedBoxes: make(map[uint32]bool),
+	}
+	packer.takeBoxes(boxes)
+	defer packer.putBackUnusedBoxes(boxes)
+	p := &pallet{
+		boxes: make([]box, 0, 16),
+	}
+	packer.pack(p)
+	return p
+}
+
+func packAllBoxes(boxes chan box) []*pallet {
+	packer := &palletPacker{
+		boxes:     make([]*box, 0, len(boxes)),
+		usedBoxes: make(map[uint32]bool),
+	}
+	packer.takeBoxes(boxes)
+	pallets := make([]*pallet, 0, len(packer.boxes))
+	for len(packer.usedBoxes) < len(packer.boxes) {
+		p := &pallet{
+			boxes: make([]box, 0, 16),
+		}
+		packer.pack(p)
+		pallets = append(pallets, p)
+	}
+	return pallets
 }
 
 var (
@@ -113,39 +137,50 @@ var (
 	maxFails = 64
 )
 
-func packPallet(p *pallet, w *warehouse) {
-	boxes := make([]*box, 0, maxBoxes)
-	usedBoxes := make(map[uint32]bool)
+type palletPacker struct {
+	boxes     []*box
+	usedBoxes map[uint32]bool
+}
 
-	for len(boxes) < cap(boxes) {
+func (p *palletPacker) takeBoxes(boxes <-chan box) {
+	for len(p.boxes) < cap(p.boxes) {
 		select {
-		case b := <-w.boxes:
+		case b := <-boxes:
 			b.x, b.y = 0, 0
-			boxes = append(boxes, &b)
+			p.boxes = append(p.boxes, &b)
 		}
 	}
-
-	defer func() {
-		for _, b := range boxes {
-			if !usedBoxes[b.id] {
-				w.boxes <- *b
-			}
+}
+func (p *palletPacker) putBackUnusedBoxes(boxes chan<- box) {
+	for _, b := range p.boxes {
+		if !p.usedBoxes[b.id] {
+			boxes <- *b
 		}
-	}()
+	}
+}
 
+func (p *palletPacker) nextBox() *box {
+	for _, b := range p.boxes {
+		if !p.usedBoxes[b.id] {
+			return b
+		}
+	}
+	return nil
+}
+func (p *palletPacker) pack(pal *pallet) {
 	fails := 0
 	for {
 		ok := false
-		b := chooseBox(boxes, usedBoxes)
+		b := p.nextBox()
 		if b == nil {
 			return
 		}
 
-		if len(p.boxes) == 0 {
+		if len(pal.boxes) == 0 {
 			ok = true
 		}
-		if len(p.boxes) > 0 && len(p.boxes) <= 4 {
-			pb := p.boxes[len(p.boxes)-1]
+		if len(pal.boxes) > 0 && len(pal.boxes) <= 4 {
+			pb := pal.boxes[len(pal.boxes)-1]
 			l := pb.x + pb.l + b.l
 			if l < 4 {
 				b.x = pb.x + pb.l
@@ -155,9 +190,8 @@ func packPallet(p *pallet, w *warehouse) {
 		}
 
 		if ok {
-			w.boxCounter.Dec()
-			usedBoxes[b.id] = true
-			p.boxes = append(p.boxes, *b)
+			p.usedBoxes[b.id] = true
+			pal.boxes = append(pal.boxes, *b)
 		} else {
 			fails++
 			if fails < maxFails {
